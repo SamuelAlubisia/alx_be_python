22ad7add56a7013531a15a0fee3f6984a2ca871b
SamuelAlubisia <salubisia@gmail.com>
2024-12-21 22:29:49 +0300

    Initial

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+size = int(input("Enter the size of the pattern: "))
+length = 0
+
+while length < size:  
+    for i in range(size):  
+        print("*", end="")  
+    print()  
+    length += 1  
+
+   
+
+    
